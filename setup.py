00674945c9089967836b3736c8c5ad2b055bc784
udincer <dincer@ucla.edu>
2021-03-31 15:17:56 -0700

    Update setup.py sge_monitor entrypoint

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="psub", 
-    version="0.1.1_alpha",
+    version="0.1.2_alpha",
     author="Tev Dincer",
     author_email="umutdincer@gmail.com",
-    description="Submit array jobs to a SGE cluster without all the suffering",
+    description="Submit and monitor array jobs on the Hoffman2 cluster with minimal configuration.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/udincer/psub",
@@ -19,5 +19,8 @@ setuptools.setup(
     ],
     python_requires='>=3.7',
     install_requires=["simple-term-menu"],
-    entry_points={'console_scripts': ['psub = psub.cli:main']}
+    entry_points={'console_scripts': [
+        'psub = psub.cli:main',
+        'sge_monitor = psub.utilities.sge_monitor:main'
+    ]}
 )
